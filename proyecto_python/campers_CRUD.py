1c410ac5c1f2163cc66bca491392a34049c9981e
Rubn Ortiz <ruben.ortiz.9@gmailcom>
2024-02-13 20:36:40 -0500

    muriendo aaaaa

--- proyecto_python/campers_CRUD.py
+++ proyecto_python/campers_CRUD.py
@@ -2,7 +2,7 @@ import json
 # Función para cargar datos desde el archivo JSON
 def cargar_datos():
     try:
-        with open('data.json', 'r') as file:
+        with open('campers.json', 'r') as file:
             data = json.load(file)
     except FileNotFoundError:
         data = []
@@ -10,11 +10,12 @@ def cargar_datos():
 
 # Función para guardar datos en el archivo JSON
 def guardar_datos(data):
-    with open('data.json', 'w') as file:
+    with open('campers.json', 'w') as file:
         json.dump(data, file, indent=2)
 
 # Función para crear un nuevo camper
 def crear_camper():
+    
     identificacion = int(input("ingresa el numero de identificación: ")) #Al inicio debes indicar donde quieres ingresar la informacion en  el JSON
     nombre = str(input("ingresa el nombre: "))
     apellido1 = str(input("ingresa el primer apellido: "))
@@ -27,7 +28,7 @@ def crear_camper():
     acudiente = str(input("ingresa el acudiente: "))
 
     data = cargar_datos()
-    nuevo_camper = {'identificacoion':identificacion, 'nombre':nombre,'apellido1':apellido1, 'apellido2':apellido2, 'direccion':direccion, 'telefono1':telefono1, 'telefono2':telefono2, 'edad':edad, 'estado':estado, 'acudiente':acudiente}
+    nuevo_camper = {'id':id,'identificacoion':identificacion, 'nombre':nombre,'apellido1':apellido1, 'apellido2':apellido2, 'direccion':direccion, 'telefono1':telefono1, 'telefono2':telefono2, 'edad':edad, 'estado':estado, 'acudiente':acudiente}
     data.append(nuevo_camper)
     guardar_datos(data)
     print("Objeto creado con éxito.")
@@ -44,38 +45,18 @@ def leer_camper():
 
 # Función para actualizar un objeto por nombre
 def actualizar_camper():
-    camper_a_actualizar = input("Ingrese el nombre del Camper a actualizar: ")
+    camper_a_actualizar = input("Ingrese el id del Camper a actualizar: ")
     data = cargar_datos()
-
-    for act_camper in data:
-        if act_camper['nombre'] == camper_a_actualizar:
-        
-            nuevo_nombre = str(input("ingresa el nombre: "))
-            nuevo_identificacion = int(input("ingresa el numero de identificación: "))
-            nuevo_apellido1 = str(input("ingresa el primer apellido: "))
-            nuevo_apellido2 = str(input("ingresa el segundo apellido: "))
-            nueva_direccion = str(input("ingresa la direccion del camper: "))
-            nuevo_telefono1 = int(input("ingresa el telefono celular: "))
-            nuevo_telefono2 = int(input("ingresa el telefono fijo: "))
-            nueva_edad = int(input("ingresa la edad: "))
-            nuevo_estado = str(input("ingrese el estado actual: "))
-            
-            act_camper['identificacion'] = nuevo_identificacion
-            act_camper['nombre'] = nueva_edad
-            act_camper['apellido1'] = nuevo_apellido1
-            act_camper['apellido2'] = nuevo_apellido2
-            act_camper['direccion'] = nueva_direccion
-            act_camper['telefono1'] = nuevo_telefono1
-            act_camper['telefono2'] = nuevo_telefono2           
-            act_camper['edad'] = nueva_edad
-            act_camper['estado'] = nuevo_estado
-            
-            guardar_datos(data)
-            print("Objeto actualizado con éxito.")
-            return
+    decisiondecamper = str(input("ingresa el id del Camper: "))
+    nombre = str(input("ingresa el nombre actualizado: "))
+    apellido1 = str(input("ingresa el primer apellido actualizado: "))
+    apellido2 = str(input("ingresa el segundo apellido actualizado: "))
+    direccion = str(input("ingresa la direccion del camper actualizado: "))
+    telefono1 = str(input("ingresa el telefono celular actualizado: "))
+    telefono2 = str(input("ingresa el telefono fijo actualizado: "))
+    edad = str(input("ingresa la edad actualizado: "))
+    acudiente = str(input("ingresa el acudiente: "))
     
-    print(f"No se encontró el objeto con el nombre {camper_a_actualizar}.")
-
 # Función para eliminar un objeto por nombre
 def eliminar_objeto():
     camper_a_eliminar = input("Ingrese el nombre del objeto a eliminar: ")
--- proyecto_python/menu.py
+++ proyecto_python/menu.py
@@ -1,58 +1,47 @@
-import json
+import campers_CRUD as crud
 import os
-import campers_CRUD
+
 contador = 0 
 categoria = {}
 while (contador != 3):
-    print("*-------------------------*")
-    print("*------Bienvenido---------*")
-    print("*-------------------------*")
-    print("1.coordinador")
-    print("2.trainer ")
-    print("3.camper")
-    print("4. salir")
-    print("--------------------------")
-    cargo = int(input("ingresa: "))
+    print("|==========================|")
+    print("|        Bienvenido        |")
+    print("|==========================|")
+    print("| 1.coordinador")
+    print("| 2.trainer ")
+    print("| 3.camper")
+    print("| 4. salir")
+    print("____________________________")
+    cargo = int(input("selecciona tu rol: "))
     if(cargo==1):
-        print("--------------------------")
+        print("_____________________________")
         print("1.Camper")
         print("2.Trainer")
         print("3.Rutas")
         print("4.notas")
         print("5.inicio")
-        print("--------------------------")
-
-        ingreso = int(input("elige"))
+        print("______________________________")
+        ingreso = int(input("elige una opcion: "))
         if (ingreso == 1):
-            print("--------------------------")
+            print("______________________________")
             print("1.Inscribir camper")
             print("2.actualizar camper")
-            print("3.Actualizar camper")
-            print("4.matricular camper")
+            print("3.matricular camper")
+            print("4.listar campers")
 
-            decisioncamper = int(input("elige"))
-            print("--------------------------")
-            if (decisioncamper==1): 
-                file.crear_camper()
-                
+            decisioncamper = int(input("elige una opcion: "))
+            print("_______________________________")
+            if (decisioncamper==1):
+                crud.crear_camper()
                         
             elif (decisioncamper==2):
-                decisiondecamper = str(input("ingresa el id del Camper: "))
-                nombre = str(input("ingresa el nombre actualizado: "))
-                apellido1 = str(input("ingresa el primer apellido actualizado: "))
-                apellido2 = str(input("ingresa el segundo apellido actualizado: "))
-                direccion = str(input("ingresa la direccion del camper actualizado: "))
-                telefono1 = str(input("ingresa el telefono celular actualizado: "))
-                telefono2 = str(input("ingresa el telefono fijo actualizado: "))
-                edad = str(input("ingresa la edad actualizado: "))
-                if (edad<18):
-                        acudiente = str(input("ingresa el acudiente: "))
+                crud.actualizar_camper()
             elif (decisioncamper==3):
                 notadeestudiante=str(input("ingresa el id del camper: "))
             elif (decisioncamper==4):
                 matriculascamper=str(input("ingresa el id del camper: "))
     if cargo==2:
-        print("--------------------------")
+        print("_____________________________")
         print("1.ingresa el horario")
         print("2.ver horario ")
         decisiondetrainers = int(input("ingresa la desicion con numeros: "))
@@ -62,9 +51,9 @@ while (contador != 3):
         if decisiondetrainers ==2:
             print ("noche")
     if cargo==3:
-        print("--------------------------")
-        print("no tienes acceso")
-        print("------------------------- ")
+        print("|==========================|")
+        print("|     No tienes acceso     |")
+        print("|==========================|")
         break
     
          
